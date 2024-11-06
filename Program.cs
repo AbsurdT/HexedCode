d859b0efaf304acd9795f96491505504c9fafcd9
AbsurdT <ezzio01@mail.ru>
2024-11-06 21:00:53 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+while (true)
+{
+    Console.WriteLine("Введите время Response Time - T3:");
+    double t3 = IsNumber();
+    Console.WriteLine("Введите объем E1 Fuel Percentage:");
+    double e1 = IsNumber();
+    string hexedT3 = ((int)Math.Round(t3, MidpointRounding.AwayFromZero) + 75).ToString("X2");
+    string hexedE1 = ((int)(Math.Round(e1,1, MidpointRounding.AwayFromZero) * 10) + 50).ToString("X2");
+    Console.WriteLine("Полученный код:");
+    Console.WriteLine($"{hexedE1}00{hexedT3}");
+    Console.WriteLine("Нажмите любую кнопку для повторного ввода");
+    Console.ReadKey();
+    Console.Clear();
+}
+static double IsNumber()
+{
+    while (true)
+    {
+        bool isNumber = double.TryParse(Regex.Replace(Console.ReadLine(), @"[.]", ","), out double input);
+        if (isNumber) return input;
+        Console.WriteLine("Попробуйте снова");
+    }
+}
+
