e6ff5b3e6c5dacd94c2fb5ff6519cbcb69ee7fdd
AbsurdT <ezzio01@mail.ru>
2024-11-07 13:20:59 +0300

    Fixed rounding

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ while (true)
     Console.WriteLine("Введите объем E1 Fuel Percentage:");
     double e1 = IsNumber();
     string hexedT3 = ((int)Math.Round(t3, MidpointRounding.AwayFromZero) + 75).ToString("X2");
-    string hexedE1 = ((int)(Math.Round(e1,1, MidpointRounding.AwayFromZero) * 10) + 50).ToString("X2");
+    string hexedE1 = ((int)Math.Floor(e1 * 10) + 50).ToString("X2");
     Console.WriteLine("Полученный код:");
     Console.WriteLine($"{hexedE1}00{hexedT3}");
     Console.WriteLine("Нажмите любую кнопку для повторного ввода");
